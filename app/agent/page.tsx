6c35e3ec5a0680d1549044942655bd8dadec9611
Ryan Green <hi@ryan.green>
2025-08-15 11:13:27 -0500

    typography

--- app/agent/page.tsx
+++ app/agent/page.tsx
@@ -4,8 +4,8 @@ export default function Page(){
   return (
     <main>
       <section style={{maxWidth: '900px', margin: '0 auto'}}>
-        <h1 style={{fontSize:'3rem', fontWeight:600, textAlign:'center', color:'#ffffff', marginBottom:'0.75rem'}}>RyanGPT ✨</h1>
-        <p style={{textAlign:'center', color:'#babfcb', marginBottom:'1.5rem'}}>Curious about my product process, design background, or specific projects? Ask below.</p>
+        <h1 style={{fontSize:'clamp(3.426rem, 3.426rem + ((1vw - 0.2rem) * 6.498), 7rem)', fontWeight:700, textAlign:'center', color:'#ffffff', marginBottom:'0.75rem', marginTop: '1.2rem'}}>RyanGPT ✨</h1>
+        <p style={{textAlign:'center', color:'#babfcb', marginBottom:'3rem'}}>Curious about my product process, design background, or specific projects? Ask below.</p>
         <AgentChat />
       </section>
     </main>
