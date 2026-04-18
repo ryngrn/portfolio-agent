de01a14810df53acb08a673c8276cdb9ce8c748e
Ryan Green <hi@ryan.green>
2026-04-17 21:15:08 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <head />
-      <body style={{ margin: 0, fontFamily: 'system-ui, -apple-system, Segoe UI, Roboto, Helvetica, Arial, sans-serif', backgroundColor: '#0f1013'}}>
+      <body style={{ margin: 0, fontFamily: 'system-ui, -apple-system, Segoe UI, Roboto, Helvetica, Arial, sans-serif', backgroundColor: '#111318'}}>
         {children}
       </body>
     </html>
